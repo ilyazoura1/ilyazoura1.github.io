50590a85971f4baedb9229150d9d485a6f18eb17
Ilya Zoura <ilyazoura1@gmail.com>
2023-08-25 12:50:25 +0330

    Update

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
 
-
-
-Btn = document.getElementById("Enter").onclick = function () {
-    document.getElementById("out").innerHTML = F + L;
-};
-F = document.getElementById("Fname");
-L = document.getElementById("Lname");
-let Nbranch = "New div";
-
-document.getElementById("gitCmd").onclick = function f() {
-    document.getElementById("gitCmd").style.display = "none";
-}
+let owner = "John";
